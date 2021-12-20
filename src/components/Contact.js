c386df5f424772b1515e5825f1729e07fdd60b9f
Otis Singfield <singfieldotis@gmail.com>
2021-12-19 19:39:46 -0500

    Small changes to verbiage

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -73,8 +73,8 @@ export default function Contact() {
             Hire Me
           </h2>
           <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
+            If you like what you see here, please don't hesitate to reach out.
+            You can get in touch using the form below.
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,5 +1,5 @@
-import React from "react";
 import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
+import React from "react";
 import { testimonials } from "../data";
 
 export default function Testimonials() {
@@ -8,7 +8,7 @@ export default function Testimonials() {
       <div className="container px-5 py-10 mx-auto text-center">
         <UsersIcon className="w-10 inline-block mb-4" />
         <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
-          Client Testimonials
+          Colleague Chatter
         </h1>
         <div className="flex flex-wrap m-4">
           {testimonials.map((testimonial) => (
